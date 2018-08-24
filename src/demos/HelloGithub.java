ce16cb1d0cdeb5cf20eac2809777d4571d485415
caifeng <645324231@qq.com>
2018-08-24 14:39:56 +0800

    add the method of getName

--- src/demos/HelloGithub.java
+++ src/demos/HelloGithub.java
@@ -2,7 +2,13 @@ package demos;
 
 public class HelloGithub {
 
+    private void getName(){
+        System.out.println("My name is Peppa!");
+    }
+
     public static void main(String args[]){
         System.out.println("Hello Github");
+        HelloGithub hg = new HelloGithub();
+        hg.getName();
     }
 }
